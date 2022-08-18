262dbbf54473983fb288f7358de3a4a87348f239
Iancarlo06 <85897963+Iancarlo06@users.noreply.github.com>
2022-08-18 16:08:55 -0500

    Update grafica.cpp

--- grafica.cpp
+++ grafica.cpp
@@ -16,14 +16,15 @@ static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr)
 
 static void do_drawing(cairo_t *cr)
 {
+    //El eje de las Y
 	cairo_stroke(cr);
   	cairo_set_source_rgb(cr, 0.3, 0, 0.4);
   	cairo_set_line_width(cr, 2.5);
   	cairo_set_font_size(cr,0);
-  	cairo_move_to(cr,100,200);
-  	cairo_line_to(cr,200,100);
-   cairo_stroke(cr);
-   cairo_save(cr);
+  	cairo_move_to(cr,20,20);
+    cairo_line_to(cr,20,480);
+    cairo_stroke(cr);
+    cairo_save(cr);
 }
 
 
@@ -38,7 +39,7 @@ int main(int argc, char *argv[])
 	gtk_container_add(GTK_CONTAINER(window), darea);
  	  	
   	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  	gtk_window_set_default_size(GTK_WINDOW(window), 400, 300); 
+  	gtk_window_set_default_size(GTK_WINDOW(window), 500, 500); 
   	gtk_window_set_title(GTK_WINDOW(window), "Graficador");
 	g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(on_draw_event), cr); 
   	gtk_widget_show_all(window);
