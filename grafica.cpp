8ccacc7fbcf2b8fc43cd2242ba9132f8d3fb1bfb
iancarlo.espinosa <iancarlo.espinosa@cimat.mx>
2022-08-22 15:52:23 -0500

    No se que estoy haciendo

---
+++ grafica.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <iostream>
+#include <cairo.h>
+#include <gtk/gtk.h>
+#include <string>
+#include <math.h>
+#include "./fparser/fparser.hh"
+
+using namespace std;
+
+static void do_drawing(cairo_t *);
+
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr)
+{
+  do_drawing(cr);
+  return FALSE;
+}
+
+static void do_drawing(cairo_t *cr)
+{
+	double a,b;
+	a = 100.0; 
+	b = 1000.0;
+    //El eje de las Y
+	cairo_stroke(cr);
+  	cairo_set_source_rgb(cr, 0, 0, 0);
+  	cairo_set_line_width(cr, 2.5);
+  	cairo_set_font_size(cr,0);
+  	cairo_move_to(cr,20,20);
+    cairo_line_to(cr,20,480);
+    cairo_stroke(cr);
+	//El eje de las X
+	cairo_stroke(cr);
+	cairo_set_font_size(cr,5);
+  	char *text;
+	double num;
+	string mystr;
+	for(int i = 0; i < 10; i++)
+	{
+		num = a + i*(b-a)/10; 
+		string mystr = to_string(num);
+		text = &mystr[0];
+		cairo_move_to(cr,20 + i*46, 490);
+		cairo_show_text(cr,text);
+	}
+	cairo_move_to(cr,20,480);
+    cairo_line_to(cr,480,480);
+	cairo_stroke(cr);
+    cairo_save(cr);
+}
+
+
+int main(int argc, char *argv[])
+{
+	cairo_t *cr;
+	GtkWidget *window;
+	GtkWidget *darea;
+	gtk_init(&argc, &argv); 	
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	darea = gtk_drawing_area_new();
+	gtk_container_add(GTK_CONTAINER(window), darea);
+ 	  	
+  	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  	gtk_window_set_default_size(GTK_WINDOW(window), 500, 500); 
+  	gtk_window_set_title(GTK_WINDOW(window), "Graficador");
+	g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(on_draw_event), cr); 
+  	gtk_widget_show_all(window);
+	gtk_main();
+	return 0;
+
+}
