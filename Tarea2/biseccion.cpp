5944935e6e27fa00ed232238b348b2edbc62cb99
Ian <iancarlo.espinosa@cimat.mx>
2022-08-27 17:54:15 -0500

    Fin de la biseccion

--- Tarea2/biseccion.cpp
+++ Tarea2/biseccion.cpp
@@ -7,22 +7,26 @@
 
 using namespace std;
 
+string func;
+FunctionParser fparser;
+
 int main(int argc, char *argv[])
 {
    double a,b, fa, fb, c, fc;
    double epsilon = 0.00000001; //Nuestra condición de paro
    int itera = 1000000; //Si no llega a cumplir la condición de paro, para que no se cicle
-   string func;
-   FunctionParser fparser;
    a = atof(argv[1]);
 	b = atof(argv[2]);
 	func = argv[3];
-	fparser.Parse(func,"x");	
 	fparser.AddConstant("pi", 3.1415926535897932);
+	fparser.AddConstant("e", 2.7182818284590452);
+	fparser.Parse(func,"x");	
+
    fa = fparser.Eval(&a);
    fb = fparser.Eval(&b);
    if(fa*fb > 0 && abs(fa - fb) > epsilon) //Los extremos no cumplen las condiciones iniciales
    {
+   	cout<<fa<<" "<<fb<<endl;
    	cout<<"Los extremos no cumplen la condicion inicial"<<endl; 
    	return 0;
    }
@@ -40,12 +44,6 @@ int main(int argc, char *argv[])
    }
    for(int i = 1; i <= itera; i++)
    {
-		if( abs(fa-fb) < epsilon) //Condición de paro
-      {
-			cout<<"El algoritmo converge y su raiz es "<<a<<endl;
-			cout<<"El número de iteraciones es "<<i<<endl;         
-         return 0;
-      }
       c = (a+b)/2;
       fc = fparser.Eval(&c);
 		if(abs(fc) < epsilon) //condición de paro
