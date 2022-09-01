0e98fabee0c370b23f7bb65399e93ee10779c0a0
iancarlo.espinosa <iancarlo.espinosa@cimat.mx>
2022-09-01 17:07:02 -0500

    Codigos finalizados

--- Tarea4/GaussSeidel.cpp
+++ Tarea4/GaussSeidel.cpp
@@ -6,34 +6,33 @@
 
 using namespace std;
 
-void Jacobi(int n, double *A, double *b, double *vv, double *vn)
+void GaussSeidel(int n, double *A, double *b, double *vv, double *vn)
 {
-    for(int i = 0; i < n; i++) *(vv + i) = (*(b + i))/(*(A + i*n + i));
-    int iter = 0, num = 0, den = 0;
+    for(int i = 0; i < n; i++) {
+        *(vv + i) = (*(b + i))/(*(A + i*n + i));
+        *(vn + i) = *(vv + i);
+    }
+    int iter = 0;
+    double tol, num, den, sum;
     while(iter < MAXITER)
     {
-        
-    }
-}
-
-void solveL(double *L, double *Y, double *B, int n) 
-{
-    for(int i = 0; i < n; i++)
-    {
-        sum = *(B + i);
-        for(int j = 0;j < i;j++) sum = sum - (*(Y + j))*(*(L + i*n + j));
-        *(Y + i) = sum/(*(L + i*n + i));
-    }
-}
-
-void solveLT(double *L, double *X, double *Y, int n) 
-{
-    for(int i = (n-1) ; i >= 0; i--)
-    {
-        sum = *(Y + i);
-        for(int j = (n-1); j > i; j--)
-            sum -= (*(X + j))*(*(L + j*n + i));
-        *(X + i) = sum/(*(L + i*n + i));
+        num = 0;
+        den = 0;
+        for(int i = 0; i < n; i++)
+        {
+            sum = 0;
+            for(int j = 0; j < n; j++)
+            {
+                if(i != j) suma = suma + (*(vn + j))*(*(A + i*n + j));
+            }
+            *(vn + i) = ((*(b + i)) - suma)/(*(A+i*n+i));
+            num = num + ( (*(vv + i)) - (*(vn + i)) )*( (*(vv + i)) - (*(vn + i)) );
+            den = den + ( (*(vn +i)) * (*(vn +i)) );
+        }
+        tol = num/den;
+        if(tol < valor) break;
+        for(int i = 0; i < n; i++) *(vv + i) = *(vn + i);
+        iter++;
     }
 }
 
@@ -45,9 +44,8 @@ int main()
    	double *A, *B, *X, *L, sum;
     A = (double *)malloc(sizeof(double)*25000000);
     B = (double *)malloc(sizeof(double)*5000);
-    X = (double *)malloc(sizeof(double)*5000);
-    Y = (double *)malloc(sizeof(double)*5000);
-    L = (double *)malloc(sizeof(double)*25000000);
+    vv = (double *)malloc(sizeof(double)*5000);
+    vn = (double *)malloc(sizeof(double)*5000);
     
     myput >> n;
     
@@ -68,10 +66,7 @@ int main()
     }
     myifs.close();
     
-    CholeskyD(A, L, n);   
-    solveL(L, Y, B, n);
-    solveLT(L, X, Y, n);
-
+    GaussSeidel(A, L, n);
     
     ofstream myo;
     myo.open("x.txt");
@@ -82,8 +77,8 @@ int main()
     
     free(A);
     free(B);
-    free(X);
-    free(L);
+    free(vn);
+    free(vv);
     return 0;
 
 }
