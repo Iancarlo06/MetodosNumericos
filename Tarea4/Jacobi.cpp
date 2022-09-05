ea174a56817be3f3d78fe3eca153fbc15ac7a691
Ian <iancarlo.espinosa@cimat.mx>
2022-09-05 10:04:48 -0500

    Se intento

--- Tarea4/Jacobi.cpp
+++ Tarea4/Jacobi.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<stdio.h>
 #include<fstream>
+#include<math.h>
 
 using namespace std;
 
-#define MAXITER 100000;
-#define valor 0.00000001;
+#define MAXITER 1000
+#define valor 0.00000001
 
 void Jacobi(int n, double *A, double *b, double *vv, double *vn)
 {
@@ -21,15 +22,19 @@ void Jacobi(int n, double *A, double *b, double *vv, double *vn)
             sum = 0;
             for(int j = 0; j < n; j++)
             {
-                if(i != j) suma = suma + (*(vv + j))*(*(A + i*n + j));
+                if(i != j) sum = sum + (*(vv + j))*(*(A + i*n + j));
             }
-            *(vn + i) = ((*(b + i)) - suma)/(*(A+i*n+i));
+            *(vn + i) = ((*(b + i)) - sum)/(*(A + i*n + i));
             num = num + ( (*(vv + i)) - (*(vn + i)) )*( (*(vv + i)) - (*(vn + i)) );
-            den = den + ( (*(vn +i)) * (*(vn +i)) );
+            den = den + ( (*(vn + i)) * (*(vn + i)) );
         }
+        num = sqrt(num);
+        den = sqrt(num);
         tol = num/den;
         if(tol < valor) break;
-        for(int i = 0; i < n; i++) *(vv + i) = *(vn + i);
+        for(int i = 0; i < n; i++){
+         	*(vv + i) = *(vn + i);		
+			}        
         iter++;
     }
 }
@@ -37,13 +42,13 @@ void Jacobi(int n, double *A, double *b, double *vv, double *vn)
 int main()
 {
 	int n,m, uno;
-    ifstream myput;
-    myput.open("A.txt");
-   	double *A, *B, *vv, *vn, sum;
-    A = (double *)malloc(sizeof(double)*25000000);
-    B = (double *)malloc(sizeof(double)*5000);
-    vv = (double *)malloc(sizeof(double)*5000);
-    vn = (double *)malloc(sizeof(double)*5000);
+   ifstream myput;
+   myput.open("mat.txt");
+   double *A, *B, *vv, *vn, sum;
+   A = (double *)malloc(sizeof(double)*25000000);
+   B = (double *)malloc(sizeof(double)*5000);
+   vv = (double *)malloc(sizeof(double)*5000);
+   vn = (double *)malloc(sizeof(double)*5000);
     
     myput >> n;
     
@@ -64,19 +69,19 @@ int main()
     }
     myifs.close();
     
-    Jacobi(n, A, b, vv, vn);
+    Jacobi(n, A, B, vv, vn);
     
     ofstream myo;
     myo.open("x.txt");
 	for(int i = 0; i < n; i++)	{
-        myo << (*(X + i)) << "\n";
+        myo << (*(vn + i)) << "\n";
     }
     myo.close();
     
     free(A);
     free(B);
-    free(X);
-    free(L);
+    free(vv);
+    free(vn);
     return 0;
 
 }
